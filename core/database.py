a1b9f3a6e9de00b5b04de4166226365d6703b78b
Hira-Iftikhar-123 <iftikharhira947@gmail.com>
2025-07-23 22:17:27 +0500

    commit

---
+++ core/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load from .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine, class_='AsyncSession', expire_on_commit=False)
+Base = declarative_base()
